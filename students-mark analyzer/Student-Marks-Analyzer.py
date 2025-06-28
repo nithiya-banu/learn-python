530d32354cc73dff1fb73386c328a45cdd5c3fa7
nithiya-banu <nithiyabanuk@gmail.com>
2025-06-28 14:45:13 +0530

    Added Student Marks Analyzer
    Practiced using input handling, conditionals, exception handling, and formatted output in Python.

---
+++ students-mark analyzer/Student-Marks-Analyzer.py
@@ -0,0 +1,28 @@
+try:
+    name = input("Enter you name:")
+    if not name.isalpha():
+        print("Please enter alphabetic value as name!")
+        exit()
+    sub1 = int(input("Enter marks for subject 1:"))
+    sub2 = int(input("Enter marks for subject 2:"))
+    sub3 = int(input("Enter marks for subject 3:"))
+    Total = sub1 +sub2 + sub3
+    Average = Total//3
+    if Average >= 90:
+        Grade = "A"
+    elif Average >= 75:
+        Grade = "B"
+    elif Average >= 60:
+        Grade = "C"
+    elif Average >=40:
+        Grade = "D"
+    else:
+        Grade = "F"
+    print(f"----------Result Summary------------\n"
+    f"        {'Name'     :<10}:  {name}                \n" 
+    f"        {'Total'    :<10}:  {Total}               \n"
+    f"        {'Average'  :<10}:  {Average}             \n"
+    f"        {'Grade'    :<10}:  {Grade}               ")
+except ValueError:
+    print("Please use alphabets for your name!")
+
